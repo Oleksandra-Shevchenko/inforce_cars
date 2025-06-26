ccd533085cfbe00bff45d2d37be93b4c9495c876
«Oleksandra-Shevchenko» <sasha.shevchenko763@gmail.com>
2025-06-26 12:25:22 +0300

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from db import run_query_from_file_with_params, run_query_without_params
-
 params = {
-    "auction_Copart": None,
-    "auction_IAAI": "IAAI",
-    "year_start": 2015,
-    "year_end": 2022,
-    "vehicle_type_1": "SUV",
-    "vehicle_type_2": None,
-    "make": "Toyota",
-    "model": None,
-    "sale_start": None,
-    "sale_end": None,
+    "auctions": ["Copart", "IAAI"],
+    "year_start": 2016,
+    "year_end": 2024,
+    "vehicle_type_1": "Automobiles",
+    "vehicle_type_2": "SUVs",
+    "make": "Honda",
+    "model": "CR-V EXL",
+    "sale_start": "2024-07-01",
+    "sale_end": "2025-06-01",
     "vehicle_condition": None
+
 }
+
 
 top_seller = run_query_from_file_with_params("queries/top_sellers.sql", params)
 all_sellers = run_query_without_params("queries/all.sql")
-# print("Top sellers:", top_seller)
+print("Top sellers:", top_seller)
 # print("All sellers:", all_sellers)
 
